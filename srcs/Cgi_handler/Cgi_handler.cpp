fd209b74ad06241c02b23798a433d917c73fa374
EnriqueSLeeK <ensebast@student.42sp.org.br>
2023-08-02 18:46:17 -0300

    added description socketpair and prototype implementation of cgi_handler

--- srcs/Cgi_handler/Cgi_handler.cpp
+++ srcs/Cgi_handler/Cgi_handler.cpp
@@ -20,12 +20,16 @@ FT::Cgi_handler& FT::Cgi_handler::operator= (const Cgi_handler & other) {
 }
 
 // Using socketpair allow us to have a two way communication
+// Instead of opening two pipes
+// Achieve interprocess communication
+// Init internal fd using socketpair
 int FT::Cgi_handler::open_socketpair() {
     if (socketpair(AF_UNIX, SOCK_STREAM, 0, socketpair_fd))
         return -1;
     return 0;
 }
 
+// If needed init external fd 
 int FT::Cgi_handler::open_socketpair(int *fd_pair) {
     if (socketpair(AF_UNIX, SOCK_STREAM, 0, fd_pair))
         return  -1;
@@ -33,8 +37,13 @@ int FT::Cgi_handler::open_socketpair(int *fd_pair) {
 }
 
 /*
-int cgi_handler(std::string parsed_req) {
-    if (parsed_req == "php") {
+int FT::Cgi_handler::cgi_handler(std::string parsed_req) {
+
+    this->open_socketpair();
+    int child_pid = fork();
+    if (child_pid == -1)
+        std::cerr << "Failure at fork()" << std::endl;
+    if (parsed_req == "some extension") {
         find_file;
         if not found return 404
         if found fork process and execute the script
@@ -45,9 +54,11 @@ int cgi_handler(std::string parsed_req) {
 }
 
 void FT::Cgi_handler::handler_php() {
-    return ;
+    dup2();
+    execve();
 }
 void FT::Cgi_handler::handle_python() {
-    return ;
+    dup2();
+    execve();
 }
 */
